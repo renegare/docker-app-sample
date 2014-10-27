f39ccd1f8ab5183e600f6cfa228ff1c46026a76e
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-10-27 18:49:38 +0000

    Another change

--- public/index.php
+++ public/index.php
@@ -2,4 +2,4 @@
 
 date_default_timezone_set('Europe/London');
 
-echo 'The time sponsered by Someone © ' . date('Y'). ' is: ' . date('Y-m-d H:i:s');
+echo 'The time sponsered by Someone Else © ' . date('Y'). ' is: ' . date('Y-m-d H:i:s');
